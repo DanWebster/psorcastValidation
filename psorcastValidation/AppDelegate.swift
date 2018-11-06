51954f1e3f53db4da08c56a85fc39d9fb702352f
Dan Webster <dan.webster@sagebase.org>
2018-11-06 11:38:20 -0800

    pem added (but ignored)

--- psorcastValidation/AppDelegate.swift
+++ psorcastValidation/AppDelegate.swift
@@ -37,12 +37,12 @@ class AppDelegate: SBAAppDelegate {
         // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
     }
 
-//    already declared in SBAAppDelegate so commenting out to avoid overwrite error
-    /*
-    func applicationDidBecomeActive(_ application: UIApplication) {
-         Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+//    already declared in SBAAppDelegate so add override to avoid overwrite error
+    
+    override func applicationDidBecomeActive(_ application: UIApplication) {
+         //Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
     }
-    */
+    
  
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
